b324b027e85ca8c0b74ca9b17ce847f6d3ee5aa3
ElMejor10 <alfredoalonsoperez59@gmail.com>
2016-06-10 12:22:44 +0200

    bucle

---
+++ fredo/spec/005-sumabucle-spec.js
@@ -0,0 +1,10 @@
+describe("suma bucle for", function(){
+it ("suma bucle", function(){
+
+var resultado="";
+for (i=0; i<10; i++){
+  resultado=resultado+i;
+  }
+  expect(resultado).toBe(55);
+});
+});
