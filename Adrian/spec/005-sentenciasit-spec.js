86055fb851d303b4f304bb066c340e9b73dafd01
AdriFer7 <adrian91fer@gmail.com>
2016-06-10 11:53:02 +0200

    Cambios

--- Adrian/spec/005-sentenciasit-spec.js
+++ Adrian/spec/005-sentenciasit-spec.js
@@ -43,5 +43,14 @@ if(nota>=5 && nota <7){
 expect(resultado).toBe("no tienes un bien");
 
 });
+it("test if sencillo con clausula or",function(){
+var nota=1;
+var resultado="";
+if(nota<2 || nota>8){
+
+  resultado="eres de los elegidos";
+}
+  expect(resultado).toBe("eres de los elegidos");
+})
 
 });
