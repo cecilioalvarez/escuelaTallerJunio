b9c52f1f785500ddfd6b1c88fdc25b665d962fa2
ElMejor10 <alfredoalonsoperez59@gmail.com>
2016-06-10 11:41:59 +0200

    operaciones2

---
+++ fredo/spec/004-sentencias-if-spec.js
@@ -0,0 +1,37 @@
+
+describe ("manejo de sentencias if", function(){
+  it ("test if sencillo", function(){
+    var nota=5;
+    var resultado="";
+    if (nota>=5) {
+      resultado="aprobado";
+    }
+    expect(resultado).toBe("aprobado");
+  });
+  it ("test if sencillo", function(){
+    var nota=3;
+    var resultado="";
+    if (nota<5) {
+      resultado="suspenso";
+    }
+    expect(resultado).toBe("suspenso");
+  });
+  it ("test if sencillo nota rango bien", function(){
+    var nota=6;
+    var resultado="";
+    if (nota>=5 && nota<7) {
+      resultado="bien";
+    }
+    expect(resultado).toBe("bien");
+});
+if ("test if con else nota rango", function(){
+  var nota=8;
+  var resultado="";
+  if (nota>=5 && nota<7) {
+
+  }else {
+    resultado="no tienes un bien";
+  }
+  expect(resultado).toBe("no tienes un bien");
+});
+});
