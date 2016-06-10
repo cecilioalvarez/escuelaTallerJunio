d6766fb6ceb35fd2a7a91deda26620d8d69bcc24
lorenaBM <lorenabenitomartin@hotmail.com>
2016-06-10 13:44:11 +0200

    bucle while

--- Lorena/spec/005-buclefor-spec.js
+++ Lorena/spec/005-buclefor-spec.js
@@ -11,8 +11,28 @@ describe("bucle for ", function() {
 
     it("test bucle for suma 5 primeros pares", function() {
         var suma = 0;
-        for (i = 0; i < 20; i = i + 2) {
+        for (i = 0; i < 10; i = i + 2) {
             suma = suma + i;
+        }
+        expect(suma).toBe(20);
+
+    });
+
+
+    it("test bucle for suma 5 primeros pares de otra forma", function() {
+        var suma = 0;
+        for (i = 0; i < 5; i++) {
+            suma = suma + i*2;
+        }
+        expect(suma).toBe(20);
+
+    });
+
+
+    it("test bucle while suma 5 primeros pares", function() {
+      var suma=0;
+      while (suma < 20) {
+            suma=suma+2;
         }
         expect(suma).toBe(20);
 
