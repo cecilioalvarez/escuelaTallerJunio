e78a5c4e5c5de5cf9f2f7233e321f3a278a3acc3
NandoAlonso <nando7211@gmail.com>
2016-06-10 11:52:46 +0200

    sentencias if

---
+++ Fernando/spec/004-sentencias-if-spec.js
@@ -0,0 +1,56 @@
+
+describe(" Manejo de sentencias If", function () {
+
+  it ("test if aprobado", function() {
+    var nota=7;
+    var resultado="";
+    if(nota>5){
+      resultado="aprobado";
+    }
+    expect(resultado).toBe("aprobado");
+  });
+
+
+  it ("test if suspenso", function() {
+    var nota=4;
+    var resultado="";
+    if(nota<5){
+      resultado="suspenso";
+    }
+    expect(resultado).toBe("suspenso");
+  });
+
+
+  it ("test entre nota y nota", function() {
+    var nota=6;
+    var resultado="";
+    if(nota>5 && nota<7){
+      resultado="bien";
+    }
+    expect(resultado).toBe("bien");
+  });
+
+
+  it ("test if con else nota rango", function() {
+    var nota=8;
+    var resultado="";
+    if(nota>=5 && nota<7){
+      resultado="bien";
+    }else {
+      resultado="suspenso";
+    }
+    expect(resultado).toBe("suspenso");
+  });
+
+
+  it ("test if sencillo con clausula OR", function() {
+    var nota=1;
+    var resultado="";
+    if(nota<2 || nota>8){
+      resultado="eres de los pocos elegidos";
+    }
+    expect(resultado).toBe("eres de los pocos elegidos");
+  });
+
+
+});
