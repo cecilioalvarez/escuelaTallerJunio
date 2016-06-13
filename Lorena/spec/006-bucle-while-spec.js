93208e284999897254b882a8c4b5db72585c170b
lorenaBM <lorenabenitomartin@hotmail.com>
2016-06-13 08:32:37 +0200

    bucle while

---
+++ Lorena/spec/006-bucle-while-spec.js
@@ -0,0 +1,14 @@
+describe("manejo de bucle while ", function() {
+
+    it("test bucle while suma 1 a 10", function() {
+        var suma = 0;
+        while (suma<10) {
+            suma = suma + 1;
+        }
+        expect(suma).toBe(10);
+
+
+
+
+    });
+  });
