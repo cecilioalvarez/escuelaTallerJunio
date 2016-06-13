b3cebd5c301b08b970abd4221d1206e4d02a1bfa
Lucialiberty <13.15.lucia@gmail.com>
2016-06-13 12:48:00 +0200

    nueva modificacion

--- LuciaBustos/spec/011-ARRAY_N_MENOR-spec.js
+++ LuciaBustos/spec/011-ARRAY_N_MENOR-spec.js
@@ -1,13 +1,12 @@
-
 describe("Manejo  de arrays", function() {
-it ("test el menor del array", function () {
-var lista = [2,2,3,5,1];
-var menor=lista[0];
-for (var i = 0; i < lista.length; i++) {
-  if (lista[i]<menor){
-    menor=lista[i];
-  }
-}
-expect(menor).toBe(1);
-});
+    it("test el menor del array", function() {
+        var lista = [2, 2, 3, 5, 1];
+        var menor = lista[0];
+        for (var i = 0; i < lista.length; i++) {
+            if (lista[i] < menor) {
+                menor = lista[i];
+            }
+        }
+        expect(menor).toBe(1);
+    });
 });
