06a03ad5220db53d362afcb436ec019ef2c47d0c
cecilioalvarez <cmac@arquitecturajava.com>
2016-06-13 13:18:48 +0200

    nuevos ficheros de arras

--- cecilio/spec/010-Arrays-spec.js
+++ cecilio/spec/010-Arrays-spec.js
@@ -4,7 +4,7 @@ describe("Manejo  de arrays", function() {
 
         var lista = [1, 2, 3];
 
-        expect(lista.length).toBe(3);
+        expect(lista.length).toBe(3)
 
 
     });
@@ -22,5 +22,21 @@ describe("Manejo  de arrays", function() {
 
     });
 
+    it("test suma items del array", function() {
+
+        var lista = [2,2,3,4,5];
+        var elemento = 2;
+        var contador=0;
+        for (var i = 0; i < lista.length; i++) {
+
+            if (elemento==lista[i]) {
+                    contador++;
+
+            }
+        }
+        expect(contador).toBe(2);
+
+
+    });
 
 });
