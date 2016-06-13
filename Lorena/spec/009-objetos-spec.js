d8bcdf89bf888fbccd5e52485a752276b9430651
lorenaBM <lorenabenitomartin@hotmail.com>
2016-06-13 11:39:28 +0200

    arrays

--- Lorena/spec/009-objetos-spec.js
+++ Lorena/spec/009-objetos-spec.js
@@ -22,21 +22,4 @@ var objeto1={nombre:"pepe"};
 expect(objeto1.nombre).toBeDefined();
 
     });
-
-
-    it("test saber longitud array", function() {
-var lista=[0,1,2,3,4,5,6,7,8,9];
-
-expect(lista.length).toBeDefined(10);
-    });
-
-    it("test array suma valores", function() {
-
-var lista=[0,1,2,3,4,5,6,7,8,9];
-var suma=0;
-for (var i = 0; i < lista.length; i++) {
-suma=suma+i;
-}
-expect(lista.length).toBe(10);
-    });
     });
