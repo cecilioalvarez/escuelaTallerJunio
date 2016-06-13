234c81585a35cbd2d398fddcb942ee82d074f69a
Adriano <miproyectoeagle@gmail.com>
2016-06-13 10:47:21 +0200

    comparaciones

--- Adriano/spec/008-paseNumeros-spec.js
+++ Adriano/spec/008-paseNumeros-spec.js
@@ -4,9 +4,11 @@ describe("Manejo de parse", function(){
 
   var cadena1=5;
   var cadena2=7;
+
   var numero1=parseInt(cadena1);
   var numero2=parseInt(cadena2);
   var suma=numero1+numero2;
+
   expect(suma).toBe(12);
 
   });
@@ -14,10 +16,12 @@ describe("Manejo de parse", function(){
 
   var cadena1=5.2;
   var cadena2=7.3;
-  var numero1=parseInt(cadena1);
-  var numero2=parseInt(cadena2);
+
+  var numero1=parseFloat(cadena1);
+  var numero2=parseFloat(cadena2);
   var suma=numero1+numero2;
+
   expect(suma).toBe(12.5);
 
-  });
+});
 });
---
+++ Adriano/spec/009-objetos-spec.js
@@ -0,0 +1,25 @@
+describe("Manejo de objetos", function(){
+
+  it("Test de identidad de objetos", function(){
+
+    var objeto1={nombre:"pepe"};
+    var objeto2={nombre:"pepe"};
+
+    expect(objeto1).not.toBe(objeto2);
+});
+it("test comparacion objetos entre si", function(){
+
+  var objeto1={nombre:"pepe"};
+  var objeto2={nombre:"pepe"};
+
+  expect(objeto1).toEqual(objeto2);
+});
+it("test contiene propiedad nombre", function(){
+
+  var objeto1={nombre:"pepe"};
+
+
+  expect(objeto1.nombre).toBeDefined();
+
+});
+});
