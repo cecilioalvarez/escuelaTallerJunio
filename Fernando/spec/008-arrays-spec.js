e9a589f4580994d5718bb8edc3b70a1caaf11f36
NandoAlonso <nando7211@gmail.com>
2016-06-13 12:26:23 +0200

    test arrays

---
+++ Fernando/spec/008-arrays-spec.js
@@ -0,0 +1,31 @@
+
+describe(" Manejo de arrays", function () {
+
+  it ("test contar array", function() {
+    var lista=[1,3,6,7];
+    expect(lista.length).toBe(4);
+  });
+
+
+  it ("test suma array", function() {
+    var suma=0;
+    var lista=[1,3,6,7];
+    for (var i=0;i<lista.length;i++){
+      suma=suma+lista[i];
+    }
+    expect(suma).toBe(17);
+  });
+
+
+  it ("test menor del array", function() {
+
+    var lista=[4,2,6,7];
+    var menor=lista[0];
+    for (var i=0;i<lista.length;i++){
+      if (menor>lista[i]){
+        menor=lista[i];
+      }
+    }
+    expect(menor).toBe(2);
+  });
+});
