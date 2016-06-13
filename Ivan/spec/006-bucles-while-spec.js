49b1cde7421e2e66ee1a6f5ae7ba271cffab0989
FlexotilComplex <ivanyea@gmail.com>
2016-06-13 08:40:50 +0200

    Modificacion
    porque si

--- Ivan/spec/006-bucles-while-spec.js
+++ Ivan/spec/006-bucles-while-spec.js
@@ -1,12 +1,13 @@
 describe("Manejo de bucle while", function() {
 
   it("test bucle for suma 10 items", function() {
-
+var i=0;
     var resultado=0;
-    while (resultado < 10) {
+    while (i < 10) {
 
 
     resultado=resultado+1;
+    i++;
 }
 expect(resultado).toBe(10);
 
