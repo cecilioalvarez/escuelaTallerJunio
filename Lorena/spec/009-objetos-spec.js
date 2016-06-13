a849ab075bc1cb1fbf4495f7fab188c0110bd796
lorenaBM <lorenabenitomartin@hotmail.com>
2016-06-13 10:49:19 +0200

    array

--- Lorena/spec/009-objetos-spec.js
+++ Lorena/spec/009-objetos-spec.js
@@ -1,20 +1,42 @@
 describe("manejo de objetos ", function() {
 
-    it("igualdad objeto consigo mismo", function() {
+    it("test de identidad de objetos", function() {
 
 var objeto1={nombre:"pepe"};
 var objeto2={nombre:"pepe"};
-expect(objeto1).toBe(objeto1);
+expect(objeto1).not.toBe(objeto2);
 
     });
 
-
-
-    it("igualdad objeto consigo mismo", function() {
+    it("test comparacion objetos entre si", function() {
 
 var objeto1={nombre:"pepe"};
 var objeto2={nombre:"pepe"};
 expect(objeto1).toEqual(objeto2);
 
+    });
+
+    it("test contiene propiedad", function() {
+var objeto1={nombre:"pepe"};
+
+expect(objeto1.nombre).toBeDefined();
+
+    });
+
+
+    it("test saber longitud array", function() {
+var lista=[0,1,2,3,4,5,6,7,8,9];
+
+expect(lista.length).toBeDefined(10);
+    });
+
+    it("test array suma valores", function() {
+
+var lista=[0,1,2,3,4,5,6,7,8,9];
+var suma=0;
+for (var i = 0; i < lista.length; i++) {
+suma=suma+i;
+}
+expect(lista.length).toBe(10);
     });
     });
