0142fdd668f26283b2ee2a1b80cf7620d46faa57
ElMejor10 <alfredoalonsoperez59@gmail.com>
2016-06-13 13:41:33 +0200

    sumar

--- fredo/spec/010-testarrays-spec.js
+++ fredo/spec/010-testarrays-spec.js
@@ -10,4 +10,16 @@ it ("test repe del array",function(){
   }
   expect(repe).toBe(2);
 });
+it ("test sumar numeros mayores de 5", function() {
+
+  var lista=[4,2,2,6,7];
+  var numero=5;
+  var suma=0;
+  for (var i=0;i<lista.length;i++){
+    if (lista[i]>numero){
+      suma=suma+lista[i];
+    }
+}
+  expect(suma).toBe(13);
+});
 });
