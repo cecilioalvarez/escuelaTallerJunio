ce1d64673b00b28e5970c5a2b23fcbfb37df9061
Sergio383 <yo_mismo383@hotmail.com>
2016-06-13 13:39:58 +0200

    sumaarray>5

--- Sergio/spec0/008manejodeobjetos.js
+++ Sergio/spec0/008manejodeobjetos.js
@@ -42,4 +42,16 @@ if (elemento==lista(i)){
 
 }
 });
+it ("test sumar numeros mayores de 5", function() {
+
+  var lista=[4,2,2,6,7];
+  var numero=5;
+  var suma=0;
+  for (var i=0;i<lista.length;i++){
+    if (lista[i]>numero){
+      suma=suma+lista[i];
+    }
+}
+  expect(suma).toBe(13);
+});
 });
