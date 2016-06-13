71a14656cb4ed04d2a80a854af0c07dab02d5a06
lorenaBM <lorenabenitomartin@hotmail.com>
2016-06-13 13:46:15 +0200

    array suma

--- Lorena/spec/010-array-spec.js
+++ Lorena/spec/010-array-spec.js
@@ -41,4 +41,20 @@ for (var i = 0; i > lista.length; i++) {
 }
 expect(contador).toBe(2);
     });
+
+
+
+    it("test array sumar todos los numeros mayores de 5", function() {
+
+var lista=[2,2,3,5,9,10];
+contador=5;
+suma=0;
+for (var i = 0; i < lista.length; i++) {
+
+  if (lista[i]>contador) {
+    suma=suma+lista[i];
+}
+}
+expect(suma).toBe(19);
+    });
     });
