9db87b99e2e985fc0d78d396031e7a9054a8825b
Lucialiberty <13.15.lucia@gmail.com>
2016-06-13 09:44:44 +0200

    todos los ficheros de lucia

---
+++ LuciaBustos/spec/003-operaciones-condecimales-spec.js
@@ -0,0 +1,16 @@
+
+describe(" Conjunto test Operaciones", function() {
+
+  it("test variables suma", function() {
+
+    var numero1=2.5;
+    var numero2=2.5;
+    var resultado=numero1+numero2;
+    expect(resultado).toBe(5);
+
+
+  });
+
+
+
+});
---
+++ LuciaBustos/spec/005-bucle_do_while .js
@@ -0,0 +1,23 @@
+
+
+
+  it("test bucle for suma 10 items do while", function() {
+
+    var i=0;
+    var suma=0;
+
+    do{
+      suma=suma+i;
+      i++;
+
+
+    }while (i<10) {
+
+    }
+
+ expect(suma).toBe(45);
+
+  });
+
+
+});
