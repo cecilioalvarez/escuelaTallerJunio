04776d15296ffdd4baa3ef08e1b086d754d20377
Sergio383 <yo_mismo383@hotmail.com>
2016-06-13 09:32:47 +0200

    gg

---
+++ Ivan/spec/007-bucles-while-spec.js
@@ -0,0 +1,16 @@
+describe("Manejo de bucle while", function() {
+
+  it("test bucle for suma 10 items", function() {
+var i=0;
+    var resultado=0;
+    while (i < 10) {
+
+
+    resultado=resultado+1;
+    i++;
+}
+expect(resultado).toBe(10);
+
+});
+
+});
