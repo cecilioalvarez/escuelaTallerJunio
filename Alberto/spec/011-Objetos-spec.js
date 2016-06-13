cde7b5dc419b78541dcde466e247d46b06a480a2
AlberN7 <gore_n7@hotmail.com>
2016-06-13 11:41:33 +0200

    Nuevos tests Alberto

--- Alberto/spec/011-Objetos-spec.js
+++ Alberto/spec/011-Objetos-spec.js
@@ -8,9 +8,43 @@ it ("igualdad entre objetos", function() {
   expect(objeto1).toBe(objeto1);
 
   });
+
 it ("igualdad entre dos objetos", function() {
   var objeto1={nombre:"pepe"};
   var objeto2={nombre:"pepe"};
   expect(objeto1).toEqual(objeto2);
+
 });
+
+it ("test identidad objetos", function() {
+
+  var objeto1={nombre:"pepe"};
+  var objeto2={nombre:"pepe"};
+  expect(objeto1).not.toBe(objeto2);
+
+  });
+  it ("test contiene propiedad nombre", function() {
+    var objeto1={nombre:"pepe"};
+
+    expect(objeto1.nombre).toBeDefined();
+
+});
+
+it ("longitud array", function() {
+  var array=[1,2,3,4,5];
+
+expect(array.length).toBe(5);
+
+});
+
+it ("suma array", function() {
+  var array=[1,2,3,4,5];
+  var suma=0;
+  for (i=0;i<array.length; i++) {
+    suma=suma+array[i];
+  }
+expect(suma).toBe(15);
+
+});
+
 });
