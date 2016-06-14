38609c804b17191aa927d121d2507806a19cfd3f
CataHollows <themomentjc@gmail.com>
2016-06-14 12:51:17 +0200

    Notas

---
+++ Jonathan/spec/014-mayoresArray-spec.js
@@ -0,0 +1,17 @@
+describe("test array", function() {
+
+    it("test mayores array", function() {
+        var lista=[1,2,3,4,5];
+        var lista2=[];
+        for (var i=0;i<lista.length;i++){
+          if (lista[i]>=3){
+          lista2.push(lista[i])
+        }
+        }
+
+        expect(lista2.length).toBe(3);
+    });
+
+
+
+});
