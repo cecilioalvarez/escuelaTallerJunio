266f426dddf4b8fc226f9d803271962c2ef5ea75
Lucialiberty <13.15.lucia@gmail.com>
2016-06-14 10:10:04 +0200

    nuevos cambios

--- LuciaBustos/spec/014-sumaNumeroNegativos.js
+++ LuciaBustos/spec/014-sumaNumeroNegativos.js
@@ -32,3 +32,13 @@ console.log(lista2);
 expect(lista.length).toBe(lista2.length);
 expect(lista).toEqual(lista2);
 });
+
+it("test mayores que 3 copiar el array", function() {
+var lista=[1,2,3,4,5];
+var lista2=[];
+for (var i = 0; i <lista.length; i++) {
+  if(lista[i]>=3){
+  }
+  }
+expect(lista2.length).toBe(3);
+});
