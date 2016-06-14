5ba1176db1dccda8557ce139d4fba03c82fa814a
cecilioalvarez <cmac@arquitecturajava.com>
2016-06-14 11:58:00 +0200

    nuevos arrays al reves

--- cecilio/spec/011-Arrays-SumaNumeros-spec.js
+++ cecilio/spec/011-Arrays-SumaNumeros-spec.js
@@ -75,6 +75,37 @@ describe("Manejo  de arrays", function() {
 
     });
 
+    it("test recorrer array al reves", function() {
+
+        var lista=[1,2,3,4,5];
+        var texto="";
+        for (var i=lista.length-1;i>=0;i--) {
+
+                texto=texto+lista[i];
+
+        }
+
+        expect(texto).toEqual("54321");
+
+    });
+
+    it("test duplicar el array al reves", function() {
+
+        var lista=[1,2,3,4,5];
+        var lista2=[];
+        for (var i=lista.length-1;i>=0;i--) {
+
+                lista2.push(lista[i]);
+
+        }
+
+        expect(lista2).toEqual([5,4,3,2,1]);
+
+    });
+
+
+
+
 
 
 
