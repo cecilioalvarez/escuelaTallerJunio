b0172160eb9a35874dd919802c0dc42d74d1d15a
cecilioalvarez <cmac@arquitecturajava.com>
2016-06-14 10:25:55 +0200

    arrays y copias

---
+++ cecilio/spec/011-Arrays-SumaNumeros-spec.js
@@ -0,0 +1,84 @@
+describe("Manejo  de arrays", function() {
+
+    it("test suma numeros mayor que 5", function() {
+
+        var lista = [1, 2, 3,4,5,6];
+        var suma=0;
+        for(var i=0;i<lista.length;i++) {
+            if (lista[i]>=5) {
+                suma= suma+lista[i];
+            }
+        }
+        expect(suma).toBe(11);
+
+
+    });
+
+    it("test suma 10 numeros negativos", function() {
+
+
+        var suma=0;
+        for(var i=0;i> -10;i--) {
+            suma=suma+i;
+        }
+        expect(suma).toBe(-45);
+
+
+    });
+
+    it("test media de un array", function() {
+
+        var lista=[1,2,3,4,5];
+        var suma=0;
+        for(var i=0;i<lista.length;i++) {
+            suma=suma+lista[i];
+        }
+        var media=suma/lista.length;
+        expect(media).toBe(3);
+
+    });
+
+    it("test añadir al array", function() {
+
+        var lista=[1,2,3,4,5];
+
+        expect(lista.length).toBe(5);
+        lista.push(1);
+        expect(lista.length).toBe(6);
+
+    });
+    it("test duplicar el array", function() {
+
+        var lista=[1,2,3,4,5];
+        var lista2=[];
+        for (var i=0;i<lista.length;i++) {
+
+            lista2.push(lista[i]);
+        }
+        console.log(lista2);
+        expect(lista.length).toBe(lista2.length);
+        expect(lista).toEqual(lista2);
+
+    });
+    it("test mayores que 3 copiar el array", function() {
+
+        var lista=[1,2,3,4,5];
+        var lista2=[];
+        for (var i=0;i<lista.length;i++) {
+                if(lista[i]>=3) {
+                    lista2.push(lista[i]);
+                }
+
+        }
+
+        expect(lista2.length).toBe(3);
+
+    });
+
+
+
+
+
+
+
+});
