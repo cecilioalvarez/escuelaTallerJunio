6076628e41b13188b87039de77d7ad07341b6a5e
Lucialiberty <13.15.lucia@gmail.com>
2016-06-14 13:42:06 +0200

    NUEVOS CAMBIOS

--- LuciaBustos/spec/015-SumaNumero.js
+++ LuciaBustos/spec/015-SumaNumero.js
@@ -118,4 +118,24 @@ describe("Manejo  de arrays", function() {
     });
 
 
-});
+    it("test array notas y texto", function() {
+        var lista = [2, 4, 6, 8, 10];
+        var lista2 = [];
+        for (var i = 0; i < lista.length; i++) {
+
+            if (lista[i] <= 2) {
+
+                lista2.push("muy mal");
+            } else if (lista[i] >= 2 && lista[i] <= 4) {
+                lista2.push("mal");
+            } else if (lista[i] >= 5 && lista[i] <= 6) {
+                lista2.push("bien");
+            } else if (lista[i] >= 7 && lista[i] <= 8) {
+                lista2.push("notable");
+            } else {
+                lista2.push("sobresaliente");
+            }
+        }
+        expect(lista2).toEqual(["muy mal", "mal", "bien", "notable", "sobresaliente"]);
+    });
+  });
