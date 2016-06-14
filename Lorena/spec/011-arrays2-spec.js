5d4c5a7cffa274225d9b3ba0f93a333de74011af
lorenaBM <lorenabenitomartin@hotmail.com>
2016-06-14 13:37:32 +0200

    arrays

--- Lorena/spec/011-arrays2-spec.js
+++ Lorena/spec/011-arrays2-spec.js
@@ -72,7 +72,7 @@ describe("manejo de arrays ", function() {
     it("test recorrer array alreves bucle for de otra forma", function() {
         var lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         var texto = "";
-        for (i = lista.length-1; i >= 0; i--) {
+        for (i = lista.length - 1; i >= 0; i--) {
             texto = texto + lista[i];
         }
         expect(texto).toBe("987654321");
@@ -82,23 +82,45 @@ describe("manejo de arrays ", function() {
     it("test poner array alreves bucle for", function() {
         var lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         var lista2 = [];
-        for (i = lista.length-1; i >= 0; i--) {
+        for (i = lista.length - 1; i >= 0; i--) {
 
             lista2.push(lista[i]);
             //console.log(lista2);
         }
-        expect(lista2).toEqual([9,8,7,6,5,4,3,2,1]);
+        expect(lista2).toEqual([9, 8, 7, 6, 5, 4, 3, 2, 1]);
     });
 
 
-it("test array con items al cuadrado", function() {
-    var lista = [1, 2, 3];
-    var lista2 = [];
-    for (i = 0; i < lista.length; i++) {
+    it("test array con items al cuadrado", function() {
+        var lista = [1, 2, 3];
+        var lista2 = [];
+        for (i = 0; i < lista.length; i++) {
 
-        lista2.push(lista[i]*2);
-        //console.log(lista2);
-    }
-    expect(lista2).toEqual([2,4,6]);
-});
+            lista2.push(lista[i] * 2);
+            //console.log(lista2);
+        }
+        expect(lista2).toEqual([2, 4, 6]);
+    });
+
+
+    it("test array notas y texto", function() {
+        var lista = [2, 4, 6, 8, 10];
+        var lista2 = [];
+        for (var i = 0; i < lista.length; i++) {
+
+            if (lista[i] <= 2) {
+
+                lista2.push("muy mal");
+            } else if (lista[i] >= 2 && lista[i] <= 4) {
+                lista2.push("mal");
+            } else if (lista[i] >= 5 && lista[i] <= 6) {
+                lista2.push("bien");
+            } else if (lista[i] >= 7 && lista[i] <= 8) {
+                lista2.push("notable");
+            } else {
+                lista2.push("sobresaliente");
+            }
+        }
+        expect(lista2).toEqual(["muy mal", "mal", "bien", "notable", "sobresaliente"]);
+    });
 });
