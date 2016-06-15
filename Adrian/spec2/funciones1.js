538577a5e110080c3e1901f5dd2150893c73489f
AdriFer7 <adrian91fer@gmail.com>
2016-06-15 12:17:50 +0200

    Arrays

---
+++ Adrian/spec2/funciones1.js
@@ -0,0 +1,17 @@
+var suma=function suma(a,b){
+
+  return a+b;
+};
+var resta=function resta(a,b){
+  return a-b;
+};
+var multi=function multi(a,b){
+  return a*b;
+};
+var divi=function divi(a,b){
+  return a/b;
+};
+exports.suma=suma;
+exports.resta=resta;
+exports.multi=multi;
+exports.divi=divi;
