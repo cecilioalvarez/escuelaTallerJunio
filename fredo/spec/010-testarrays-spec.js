28b155174ac6c8b9b915b79f6bee2a11f7d88a31
ElMejor10 <alfredoalonsoperez59@gmail.com>
2016-06-15 08:41:32 +0200

    suma arrays

--- fredo/spec/010-testarrays-spec.js
+++ fredo/spec/010-testarrays-spec.js
@@ -79,4 +79,32 @@ it ("test recorrer array al reves", function() {
 }
   expect(texto).toEqual("54321");
 });
+it ("test duplicar array al reves", function() {
+
+  var lista1=[1,2,3,4,5];
+  var lista2=[];
+  for(var i=lista1.length-1; i>=0; i--){
+  lista2.push(lista1[i]);
+}
+  expect(lista2).toEqual([5,4,3,2,1]);
+});
+it ("test nuevo array *2", function() {
+
+  var lista1=[1,2,3,4,5];
+  var lista2=[];
+  for(var i=0; i<lista1.length; i++){
+  lista2.push(lista1[i]*2);
+}
+  expect(lista2).toEqual([2,4,6,8,10]);
+});
+it ("test crear array suma dde arrays", function() {
+
+  var lista1=[1,2,3,4,5];
+  var lista2=[1,2,3,4,5];
+  var lista3=[];
+  for(var i=0; i<lista1.length; i++){
+  lista3.push(lista1[i]+lista2[i]);
+}
+  expect(lista3).toEqual([2,4,6,8,10]);
+});
 });
