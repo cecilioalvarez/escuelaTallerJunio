2c866d20e9d232b28a41a0875e2d9ff77835e294
lorenaBM <lorenabenitomartin@hotmail.com>
2016-06-15 12:02:32 +0200

    function

--- Lorena/spec2/003-spec.js
+++ Lorena/spec2/003-spec.js
@@ -1,33 +1,42 @@
+
+
 var misfunciones=require("./funciones2.js");
 
 //creo el test
 describe("Funciones basicas", function() {
 
-  var numero1;
-var array1;
+    var numero1;
+    var array1;
+    var array2;
+    beforeEach(function() {
+        array1 = [1, 2, 3];
+        array2=[1,1,1];
+        numero1 = 4;
+    });
+    it("test suma n numeros", function() {
 
-  beforeEach(function(){
-array1=[1,2,3];
-    numero1=4;
-  });
+        expect(misfunciones.sumaNumeros(numero1)).toBe(6);
+    });
 
-  it ("test suma n numeros", function(){
+    it("test factorial n primeros numeros", function() {
 
-    expect(misfunciones.sumaNumeros(numero1)).toBe(6);
-  });
+        expect(misfunciones.factorial(numero1)).toBe(24);
+    });
 
-  it ("test factorial n primeros numeros", function(){
+    it("test elementos en el array", function() {
 
-    expect(misfunciones.factorial(numero1)).toBe(24);
-  });
+        expect(misfunciones.arrayLongitud(array1)).toBe(3);
+    });
 
-it ("test elementos en el array", function(){
+    it("test suma elementos en el array", function() {
 
-  expect(misfunciones.arrayLongitud(array1)).toBe(3);
-});
+        expect(misfunciones.arraySuma(array1)).toBe(6);
+    });
 
-it ("test suma elementos en el array", function(){
 
-  expect(misfunciones.arraySuma(array1)).toBe(6);
-});
+    it("test la suma mayor de dos arrays", function() {
+
+        expect(misfunciones.arraySumaMayor(array1,array2)).toBe(6);
+    });
+
 });
--- Lorena/spec2/funciones2.js
+++ Lorena/spec2/funciones2.js
@@ -1,39 +1,60 @@
 var sumaNumeros = function sumar(numero) {
 
-var suma=0;
-for (var i = 0; i < numero; i++) {
+    var suma = 0;
+    for (var i = 0; i < numero; i++) {
         suma = suma + i;
     }
- return suma;
+    return suma;
 };
 
 
 var productoNumeros = function productoNumeros(numero) {
 
-var producto=1;
-for (var i = 1; i <= numero; i++) {
+    var producto = 1;
+    for (var i = 1; i <= numero; i++) {
         producto = producto * i;
     }
- return producto;
+    return producto;
 };
 
 
 var arrayLongitud = function arrayLongitud(miarray) {
 
- return miarray.length;
+    return miarray.length;
 };
 
 
 var arraySuma = function arraySuma(sumar) {
 
-var suma=0;
-for (var i = 0; i < sumar.length; i++) {
-suma=suma+sumar[i];
+    var suma = 0;
+    for (var i = 0; i < sumar.length; i++) {
+        suma = suma + sumar[i];
+    }
+    return suma;
+};
+
+var arraySumaMayor = function arraySumaMayor(array1, array2) {
+
+var suma = 0;
+var suma2 = 0;
+for (var i = 0; i < array1.length; i++) {
+    suma = suma + array1[i];
 }
-return suma;
+
+for (var j = 0; j < array2.length; j++) {
+    suma2 = suma2 + array2[j];
+}
+
+if (suma2 > suma) {
+    return suma2;
+} else {
+    return suma;
+}
+
 };
 
 exports.sumaNumeros = sumaNumeros;
 exports.factorial = productoNumeros;
 exports.arrayLongitud = arrayLongitud;
 exports.arraySuma = arraySuma;
+exports.arraySumaMayor = arraySumaMayor;
