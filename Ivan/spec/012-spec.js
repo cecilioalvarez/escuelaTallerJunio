0c460ec0608cc85fa5cb8fd48ce1b688c99b6380
FlexotilComplex <ivanyea@gmail.com>
2016-06-16 08:30:57 +0200

    Media de array
    pues eso

--- Ivan/spec/012-spec.js
+++ Ivan/spec/012-spec.js
@@ -14,4 +14,15 @@ it ("test negativo", function() {
     else{resultado=resultado+"negativo";}
 expect(resultado).toBe("negativo");
 });
+it("test media array", function() {
+
+var lista=[4,5,1,3,8];
+var suma=0;
+for (var i = 0; i < lista.length; i++) {
+suma=suma+lista[i]/5;
+}
+expect(suma).toBe(4.2);
+});
+
+
 });
