8a3be1e6e8b763b5002bf9d9b4742c7a2b2a04ea
Lu_espada <lespadag86@gmail.com>
2016-06-16 11:20:30 +0200

    tareas -jueves

---
+++ Lucía Espada/spec/010-tareas-spec.js
@@ -0,0 +1,53 @@
+describe("Manejo de Objetos", function() {
+
+it ("test positivo", function() {
+  var numero=2;
+  var resultado="";
+    if (numero>=0){resultado=resultado+"positivo";}
+    else{resultado=resultado+"negativo";}
+expect(resultado).toBe("positivo");
+
+});
+
+it ("test negativo", function() {
+  var numero=-6;
+  var resultado="";
+    if (numero>=0){resultado=resultado+"positivo";}
+    else{resultado=resultado+"negativo";}
+expect(resultado).toBe("negativo");
+});
+
+it("test valor medio array", function() {
+
+var lista=[4,5,7,9];
+var suma=0;
+for (var i = 0; i < lista.length; i++) {
+suma=suma+lista[i]/4;
+}
+expect(suma).toBe(6.25);
+});
+
+it ("test el mayor y menor en un array", function() {
+  var lista=[2,3,6,8];
+  var lista2=[];
+for (var i = 0; i < lista.length; i++) {
+    if (lista[i]>=8){lista2.push(lista[i]);
+    }else if(lista[i]<=2){lista2.push(lista[i]);}
+
+  }
+expect(lista2).toEqual([2,8]);
+});
+
+it ("test la media de la suma ", function() {
+  var lista=[2,3,6,8];
+  var lista2=[0];
+for (var i = 0; i < lista.length; i++) {
+    if (lista[i]>=8){lista2.push(lista[i]);
+    }
+    else if(lista[i]<=2){lista2.push(lista[i]);
+    }
+  }
+expect(lista2).toEqual([2,8]);
+});
+
+});
